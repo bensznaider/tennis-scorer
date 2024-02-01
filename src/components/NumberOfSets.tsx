88560045344ede3b8d4d9a6475b61993139f09ff
bensznaider <bensznaider@yahoo.com.ar>
2024-02-01 18:29:25 -0300

    AlertMessage added in NumberOfSets and WhoServesFirst

--- src/components/NumberOfSets.tsx
+++ src/components/NumberOfSets.tsx
@@ -7,32 +7,107 @@ interface NumberOfSetsProps {
   setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const NumberOfSets: React.FC<NumberOfSetsProps> = ({setFlowStep, setNumberOfSets, setLoading}) => {
+const NumberOfSets: React.FC<NumberOfSetsProps> = ({
+  setFlowStep,
+  setNumberOfSets,
+  setLoading,
+}) => {
   const [selectedOption, setSelectedOption] = useState<number>(0);
+  const [alertMessage, setAlertMessage] = useState<boolean>(false);
 
   const handleSetsSelector = (option: number) => {
-    setSelectedOption(option)
+    setSelectedOption(option);
   };
 
- const handleContinue = () => {
-   setNumberOfSets(selectedOption);
-   setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      setFlowStep(2);
-    }, 1000);
-  }
-  
+  const handleContinue = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (selectedOption) {
+      setNumberOfSets(selectedOption);
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setFlowStep(2);
+      }, 1000);
+    } else {
+      setAlertMessage(true);
+      setTimeout(() => {
+        setAlertMessage(false);
+      }, 1500);
+    }
+  };
+
   return (
-    <div >
+    <div>
       <form onSubmit={handleContinue}>
         <h1>Select number of sets.</h1>
-        <div style={{display:"flex"}}>
-          <div className={selectedOption === 1 ? "selected-option" : "not-selected-option"} style={{margin:"0.5rem", width:"4rem"}} onClick={() => handleSetsSelector(1)}>1 set <span style={{position:"absolute", marginLeft:"0.3rem", color:"lightgreen"}}>{selectedOption === 1 ? '✔' : null}</span></div>
-          <div className={selectedOption === 3 ? "selected-option" : "not-selected-option"} style={{margin:"0.5rem", width:"4rem"}} onClick={() => handleSetsSelector(3)}>3 sets <span style={{position:"absolute", marginLeft:"0.3rem", color:"lightgreen"}}>{selectedOption === 3 ? '✔' : null}</span></div>
-          <div className={selectedOption === 5 ? "selected-option" : "not-selected-option"} style={{margin:"0.5rem", width:"4rem"}} onClick={() => handleSetsSelector(5)}>5 sets <span style={{position:"absolute", marginLeft:"0.3rem", color:"lightgreen"}}>{selectedOption === 5 ? '✔' : null}</span></div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div
+            className={
+              selectedOption === 1 ? "selected-option" : "not-selected-option"
+            }
+            style={{ margin: "0.5rem", width: "4rem" }}
+            onClick={() => handleSetsSelector(1)}
+          >
+            1 set{" "}
+            <span
+              style={{
+                position: "absolute",
+                marginLeft: "0.3rem",
+                color: "lightgreen",
+              }}
+            >
+              {selectedOption === 1 ? "✔" : null}
+            </span>
+          </div>
+          <div
+            className={
+              selectedOption === 3 ? "selected-option" : "not-selected-option"
+            }
+            style={{ margin: "0.5rem", width: "4rem" }}
+            onClick={() => handleSetsSelector(3)}
+          >
+            3 sets{" "}
+            <span
+              style={{
+                position: "absolute",
+                marginLeft: "0.3rem",
+                color: "lightgreen",
+              }}
+            >
+              {selectedOption === 3 ? "✔" : null}
+            </span>
+          </div>
+          <div
+            className={
+              selectedOption === 5 ? "selected-option" : "not-selected-option"
+            }
+            style={{ margin: "0.5rem", width: "4rem" }}
+            onClick={() => handleSetsSelector(5)}
+          >
+            5 sets{" "}
+            <span
+              style={{
+                position: "absolute",
+                marginLeft: "0.3rem",
+                color: "lightgreen",
+              }}
+            >
+              {selectedOption === 5 ? "✔" : null}
+            </span>
+          </div>
         </div>
         <ContinueButton />
+        {alertMessage && (
+          <p
+            style={{
+              position: "absolute",
+              marginTop: "0.6rem",
+              marginLeft: "2.5rem",
+            }}
+          >
+            Please select an option to continue.
+          </p>
+        )}
       </form>
     </div>
   );
--- src/components/WhoServesFirst.tsx
+++ src/components/WhoServesFirst.tsx
@@ -9,48 +9,130 @@ interface WhoServesFirstProps {
   setLoading: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const WhoServesFirst: React.FC<WhoServesFirstProps> = ({player1, player2, setServer, setFlowStep, setLoading}) => {
+const WhoServesFirst: React.FC<WhoServesFirstProps> = ({
+  player1,
+  player2,
+  setServer,
+  setFlowStep,
+  setLoading,
+}) => {
   const [selectedOption, setSelectedOption] = useState<string>("");
-  const [randomButtonContent, setRandomButtonContent] = useState<React.ReactNode>("Random");
+  const [randomButtonContent, setRandomButtonContent] =
+    useState<React.ReactNode>("Random");
+  const [alertMessage, setAlertMessage] = useState<boolean>(false);
 
   const handleSetsSelector = (option: string) => {
-    setSelectedOption(option)
+    setSelectedOption(option);
   };
 
   const handleRandomChoice = () => {
-    const randomNumber = Math.random()
-    setRandomButtonContent(<img
-      src="assets/tennis-ball-svgrepo-com.svg"
-      alt="Tennis ball picture"
-      id="spinning-ball"
-      style={{height:"20px",marginTop:"2px"}}
-    />);
+    const randomNumber = Math.random();
+    setRandomButtonContent(
+      <img
+        src="assets/tennis-ball-svgrepo-com.svg"
+        alt="Tennis ball picture"
+        id="spinning-ball"
+        style={{ height: "20px", marginTop: "2px" }}
+      />
+    );
     setTimeout(() => {
       setRandomButtonContent("Random");
-      randomNumber < 0.5 ? setSelectedOption("player1") : setSelectedOption("player2")
+      randomNumber < 0.5
+        ? setSelectedOption("player1")
+        : setSelectedOption("player2");
     }, 1000);
-
   };
 
- const handleContinue = () => {
-  setServer(selectedOption);
-  setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-      setFlowStep(3);
-    }, 1000);
-  }
-  
+  const handleContinue = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (selectedOption) {
+      setServer(selectedOption);
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        setFlowStep(3);
+      }, 1000);
+    } else {
+      setAlertMessage(true);
+      setTimeout(() => {
+        setAlertMessage(false);
+      }, 1500);
+    }
+  };
+
   return (
-    <div >
+    <div>
       <form onSubmit={handleContinue}>
         <h1>Select who starts serving.</h1>
-        <div style={{display:"flex", marginBottom:"0.5rem"}}>
-        <div className={selectedOption === "player1" ? "selected-option" : "not-selected-option"} style={{margin:"0.5rem", width:"7rem"}} onClick={() => handleSetsSelector("player1")}>{player1} <span style={{position:"absolute", marginLeft:"0.3rem", color:"lightgreen"}}>{selectedOption === "player1" ? '✔' : null}</span></div>
-        <div className={selectedOption === "player2" ? "selected-option" : "not-selected-option"} style={{margin:"0.5rem", width:"7rem"}} onClick={() => handleSetsSelector("player2")}>{player2} <span style={{position:"absolute", marginLeft:"0.3rem", color:"lightgreen"}}>{selectedOption === "player2" ? '✔' : null}</span></div>
+        <div style={{ display: "flex", marginBottom: "0.5rem" }}>
+          <div
+            className={
+              selectedOption === "player1"
+                ? "selected-option"
+                : "not-selected-option"
+            }
+            style={{ margin: "0.5rem", width: "7rem" }}
+            onClick={() => handleSetsSelector("player1")}
+          >
+            {player1}{" "}
+            <span
+              style={{
+                position: "absolute",
+                marginLeft: "0.3rem",
+                color: "lightgreen",
+              }}
+            >
+              {selectedOption === "player1" ? "✔" : null}
+            </span>
+          </div>
+          <div
+            className={
+              selectedOption === "player2"
+                ? "selected-option"
+                : "not-selected-option"
+            }
+            style={{ margin: "0.5rem", width: "7rem" }}
+            onClick={() => handleSetsSelector("player2")}
+          >
+            {player2}{" "}
+            <span
+              style={{
+                position: "absolute",
+                marginLeft: "0.3rem",
+                color: "lightgreen",
+              }}
+            >
+              {selectedOption === "player2" ? "✔" : null}
+            </span>
+          </div>
         </div>
-        <div className="continue-button" style={{textAlign:"center",marginLeft:"auto", marginRight:"auto",marginBottom:"1rem", height:"1.5rem", width:"5rem", backgroundColor:"black"}} onClick={handleRandomChoice}>{randomButtonContent}</div>
+        <div
+          className="continue-button"
+          style={{
+            textAlign: "center",
+            marginLeft: "auto",
+            marginRight: "auto",
+            marginBottom: "1rem",
+            height: "1.5rem",
+            width: "5rem",
+            backgroundColor: "black",
+          }}
+          onClick={handleRandomChoice}
+        >
+          {randomButtonContent}
+        </div>
         <ContinueButton />
+        {alertMessage && (
+          <p
+            style={{
+              position: "absolute",
+              marginTop: "0.6rem",
+              marginLeft: "2rem",
+            }}
+          >
+            Please select an option to continue.
+          </p>
+        )}
       </form>
     </div>
   );
